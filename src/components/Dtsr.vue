c3ba2c76936ac78c81708fa187dd78e900a76a55
王凯 <137853302@qq.com>
2019-03-17 22:38:52 +0800

    1

--- src/components/Dtsr.vue
+++ src/components/Dtsr.vue
@@ -35,24 +35,15 @@
               :key="index"
             >{{xzt_label(index)}}</el-radio>
           </el-form-item>
-          <el-form-item label="分值:">
-            <el-row type="flex">
-              <el-col :span="5">
-                <el-input-number v-model="xzt_fz" controls-position="right" :min="0" :max="50"></el-input-number>
-              </el-col>
-              <el-col :span="5">
-                <div>难易度:
-                  <el-select v-model="value">
-                    <el-option
-                      v-for="item in nyd"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value"
-                    ></el-option>
-                  </el-select>
-                </div>
-              </el-col>
-            </el-row>
+          <el-form-item label="难易度:">
+            <el-select v-model="value">
+              <el-option
+                v-for="item in nyd"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="xzt_submit">提交</el-button>
@@ -88,31 +79,22 @@
             </el-row>
           </el-form-item>
           <el-form-item label="答案">
-            <el-radio
+            <el-checkbox
               v-for="(xx,index) in dxt.dxt_xx"
               v-model="dxt_radio"
               :label="dxt_label(index)"
               :key="index"
-            >{{dxt_label(index)}}</el-radio>
+            >{{dxt_label(index)}}</el-checkbox>
           </el-form-item>
-          <el-form-item label="分值:">
-            <el-row type="flex">
-              <el-col :span="5">
-                <el-input-number v-model="dxt_fz" controls-position="right" :min="0" :max="50"></el-input-number>
-              </el-col>
-              <el-col :span="5">
-                <div>难易度:
-                  <el-select v-model="value">
-                    <el-option
-                      v-for="item in nyd"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value"
-                    ></el-option>
-                  </el-select>
-                </div>
-              </el-col>
-            </el-row>
+          <el-form-item label="难易度:">
+            <el-select v-model="value">
+              <el-option
+                v-for="item in nyd"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="dxt_submit">提交</el-button>
@@ -140,31 +122,28 @@
           >
             <el-row type="flex" justify="space-around">
               <el-col :span="22">
-                <el-input v-model="xx.text" type="textarea" autosize clearable></el-input>
+                <el-input
+                  v-model="xx.text"
+                  type="textarea"
+                  autosize
+                  clearable
+                  placeholder="多个答案请用#分隔"
+                ></el-input>
               </el-col>
               <el-col :span="1">
                 <el-button @click.prevent="removeTkt(xx)" type="danger" icon="el-icon-delete"></el-button>
               </el-col>
             </el-row>
           </el-form-item>
-          <el-form-item label="分值:">
-            <el-row type="flex">
-              <el-col :span="5">
-                <el-input-number v-model="tkt_fz" controls-position="right" :min="0" :max="50"></el-input-number>
-              </el-col>
-              <el-col :span="5">
-                <div>难易度:
-                  <el-select v-model="value">
-                    <el-option
-                      v-for="item in nyd"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value"
-                    ></el-option>
-                  </el-select>
-                </div>
-              </el-col>
-            </el-row>
+          <el-form-item label="难易度:">
+            <el-select v-model="value">
+              <el-option
+                v-for="item in nyd"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="tkt_submit">提交</el-button>
@@ -176,7 +155,6 @@
       <el-tab-pane label="判断题" name="fourd">
         <el-form ref="tkt" :model="tkt" label-position="right">
           <h4>判断题题干</h4>
-
           <UE
             v-model="pdt_defaultMsg"
             :writeMsg="pdt_defaultMsg"
@@ -184,12 +162,21 @@
             :id="pdt_UE"
             ref="pdt_ue"
           ></UE>
-
           <el-form-item>
             <el-radio-group v-model="pdt_radio">
               <el-radio-button label="对"></el-radio-button>
               <el-radio-button label="错"></el-radio-button>
             </el-radio-group>
+          </el-form-item>
+          <el-form-item label="难易度:">
+            <el-select v-model="value">
+              <el-option
+                v-for="item in nyd"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="pdt_submit">提交</el-button>
@@ -212,6 +199,16 @@
           <el-form-item>
             <el-input type="textarea" autosize placeholder="请输入简答题答案" v-model="jdt_da"></el-input>
           </el-form-item>
+          <el-form-item label="难易度:">
+            <el-select v-model="value">
+              <el-option
+                v-for="item in nyd"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
+          </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="jdt_submit">提交</el-button>
             <el-button @click="jdt_dialogVisible">预览</el-button>
@@ -230,10 +227,19 @@
             :id="zht_UE"
             ref="zht_ue"
           ></UE>
-
           <el-form-item v-for="(tm,index) in zht" :prop="'zht.'+index+'.tm'" :key="index">
             <el-input autosize placeholder="请输入综合题题目" v-model="tm.tm"></el-input>
             <el-input type="textarea" autosize placeholder="请输入综合题答案" v-model="tm.da"></el-input>
+          </el-form-item>
+          <el-form-item label="难易度:">
+            <el-select v-model="value">
+              <el-option
+                v-for="item in nyd"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="zht_submit">提交</el-button>
@@ -288,6 +294,9 @@
         </el-row>
       </span>
     </el-dialog>
+    <el-dialog title="提交" :visible.sync="submit_Dialog" width="30%" center>
+      <span>{{submit_log}}</span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -297,6 +306,8 @@ export default {
   name: "Dtsr",
   data() {
     return {
+      submit_Dialog: false,
+      submit_log: "",
       activeName: "first",
       value: "",
       nyd: [
@@ -319,7 +330,6 @@ export default {
       ],
 
       xzt_UE: "xzt_UE",
-      xzt_fz: "0",
       xzt: {
         name: "",
         tm: "",
@@ -350,7 +360,6 @@ export default {
       xzt_radio: "A",
 
       dxt_UE: "dxt_UE",
-      dxt_fz: "0",
       dxt: {
         name: "",
         tm: "",
@@ -378,7 +387,7 @@ export default {
         autoClearinitialContent: true
       },
       dxt_Dialog: false,
-      dxt_radio: "A",
+      dxt_radio: [],
 
       tkt_defaultMsg: "",
       tkt_Dialog: false,
@@ -393,8 +402,6 @@ export default {
         name: "",
         tkt_xx: []
       },
-      tkt_fz: "0",
-
       pdt_defaultMsg: "",
       pdt_Dialog: false,
       pdt_config: {
@@ -459,24 +466,23 @@ export default {
       console.log(this.xzt.xzt_xx);
     },
     xzt_submit() {
-      console.log(this.$refs.xzt_ue.getUEContent());
-      console.log(this.xzt.xzt_xx);
-      console.log(this.xzt_radio);
-      console.log(this.xzt_fz);
-      console.log(this.value);
-      var tg = this.xzt.xzt_xx;
       this.$http
         .post("/api/xzt", {
           tigan: this.$refs.xzt_ue.getUEContent(),
           xx: this.xzt.xzt_xx,
           da: this.xzt_radio,
-          fz: this.xzt_fz,
           nyd: this.value
         })
         .then(function(res) {
-          if(res.body = "true"){
-            this.$refs.xzt.resetFields()
-            this.$refs.xzt_ue.setUEContent("")
+          if ((res.body = "true")) {
+            this.submit_log = "提交成功";
+            this.submit_Dialog = true;
+            this.$refs.xzt.resetFields();
+            this.$refs.xzt_ue.setUEContent("");
+            this.value = "";
+          } else {
+            this.submit_log = res.body;
+            this.submit_Dialog = true;
           }
         });
     },
@@ -507,7 +513,26 @@ export default {
       console.log(this.dxt.dxt_xx);
     },
     dxt_submit() {
-      
+      this.$http
+        .post("/api/dxt", {
+          tigan: this.$refs.dxt_ue.getUEContent(),
+          xx: this.dxt.dxt_xx,
+          da: this.dxt_radio,
+          nyd: this.value
+        })
+        .then(function(res) {
+          if ((res.body = "true")) {
+            this.submit_log = "提交成功";
+            this.submit_Dialog = true;
+            this.$refs.dxt.resetFields();
+            this.$refs.dxt_ue.setUEContent("");
+            this.dxt_radio = [];
+            this.value = "";
+          } else {
+            this.submit_log = res.body;
+            this.submit_Dialog = true;
+          }
+        });
     },
     dxt_dialogVisible() {
       this.dxt_defaultMsg = this.$refs.dxt_ue.getUEContent();
@@ -515,14 +540,32 @@ export default {
       this.dxt_Dialog = true;
     },
     tkt_submit() {
+      this.$http
+        .post("/api/tkt", {
+          tigan: this.$refs.tkt_ue.getUEContent(),
+          da: this.tkt.tkt_xx,
+          nyd: this.value
+        })
+        .then(function(res) {
+          if ((res.body = "true")) {
+            this.submit_log = "提交成功";
+            this.submit_Dialog = true;
+            this.$refs.tkt.resetFields();
+            this.$refs.tkt_ue.setUEContent("");
+            this.tkt.tkt_xx = [];
+            this.value = "";
+          } else {
+            this.submit_log = res.body;
+            this.submit_Dialog = true;
+          }
+        });
       console.log(this.$refs.tkt_ue.getUEContent());
     },
     tkt_add() {
       this.tkt.tkt_xx.push({
         text: ""
       });
-      var kg = "{____}";
-      this.$refs.tkt_ue.insertHtml("______");
+      this.$refs.tkt_ue.insertHtml("(______)");
       console.log(this.$refs.tkt_ue.getUEContent());
     },
     tkt_dialogVisible() {
@@ -535,26 +578,81 @@ export default {
         this.tkt.tkt_xx.splice(index, 1);
       }
     },
-    pdt_submit() {},
+    pdt_submit() {
+      this.$http
+        .post("/api/pdt", {
+          tigan: this.$refs.pdt_ue.getUEContent(),
+          da: this.pdt_radio,
+          nyd: this.value
+        })
+        .then(function(res) {
+          if ((res.body = "true")) {
+            this.submit_log = "提交成功";
+            this.submit_Dialog = true;
+            this.$refs.pdt.resetFields();
+            this.$refs.pdt_ue.setUEContent("");
+            this.value = "";
+          } else {
+            this.submit_log = "提交失败";
+            this.submit_Dialog = true;
+          }
+        });
+    },
     pdt_dialogVisible() {
       this.pdt_defaultMsg = this.$refs.pdt_ue.getUEContent();
       this.pdt_Dialog = true;
       console.log(this.pdt_defaultMsg);
       console.log(this.$refs.pdt_ue.getUEContent());
     },
-    jdt_submit() {},
+    jdt_submit() {
+      this.$http
+        .post("/api/jdt", {
+          tigan: this.$refs.jdt_ue.getUEContent(),
+          da: this.jdt_da,
+          nyd: this.value
+        })
+        .then(function(res) {
+          if ((res.body = "true")) {
+            this.submit_log = "提交成功";
+            this.submit_Dialog = true;
+            this.$refs.jdt_ue.setUEContent("");
+            this.jdt_da = "";
+            this.value = "";
+          } else {
+            this.submit_log = res.body;
+            this.submit_Dialog = true;
+          }
+        });
+    },
     jdt_dialogVisible() {
       this.jdt_defaultMsg = this.$refs.jdt_ue.getUEContent();
       this.jdt_Dialog = true;
       console.log(this.jdt_defaultMsg);
       console.log(this.$refs.jdt_ue.getUEContent());
     },
-    zht_submit() {},
+    zht_submit() {
+      this.$http
+        .post("/api/zht", {
+          tigan: this.$refs.zht_ue.getUEContent(),
+          da: this.zht,
+          nyd: this.value
+        })
+        .then(function(res) {
+          if ((res.body = "true")) {
+            this.submit_log = "提交成功";
+            this.submit_Dialog = true;
+            this.$refs.zht_ue.setUEContent("");
+            this.zht = [{ tm: "", da: "" }];
+            this.value = "";
+          } else {
+            this.submit_log = "提交失败";
+            this.submit_Dialog = true;
+          }
+        });
+    },
     zht_dialogVisible() {
-      this.zht_defaultMsg = this.$refs.jdt_ue.getUEContent();
+      this.zht_defaultMsg = this.$refs.zht_ue.getUEContent();
       this.zht_Dialog = true;
-      console.log(this.zht_defaultMsg);
-      console.log(this.$refs.zht_ue.getUEContent());
     },
     zht_add() {
       this.zht.push({
